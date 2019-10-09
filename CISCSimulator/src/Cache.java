16431ecdee1ee247392c67ed7b5c341ff179166b
_isrinukollipoti <_isrinukollipoti@10.0.0.215>
2019-10-09 13:56:55 -0400

    Added Cache Implementation

--- CISCSimulator/src/Cache.java
+++ CISCSimulator/src/Cache.java
@@ -3,22 +3,92 @@
  */
 
 /**
- * @author cozyu
+ * @author srinukollipoti
  *
  */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
+
+
 public class Cache {
+
+
+		
+private final LinkedList<WORD> cache;
+
+private final HashMap<Long, WORD> find;
+
+private final static int DEFAULT_CACHE_SIZE = 16;
+private final int maxCacheSize;
+private long C_hit = 0, C_miss = 0, totalhits = 0;
+
+public Cache() {
+    this.cache = new LinkedList<>();
+    this.find = new HashMap<>();
+    this.maxCacheSize = DEFAULT_CACHE_SIZE;
+    this.cachestack = new LinkedList<CacheLoad>();
+}
+
+public Cache(int cacheSize) {
+    this.cache = new LinkedList<>();
+    this.find = new HashMap<>();
+    this.maxCacheSize = cacheSize;
+	  
+
+	}
+
+	public class CacheLoad {
+
+		long address;
+		WORD data;
+	
+
+		public CacheLoad(long address, WORD data) {
+			this.address = address;
+			this.data = data;
+		}
+
+		public long getMemAddress() {
+			return this.address;
+		}
+
+		public void setAddress(long address) {
+			this.address = address;
+		}
+
+		public WORD getData() {
+			return data;
+		}
+
+		public void setData(WORD data) {
+			this.data = data;
+		}
+	}
+
+	LinkedList<CacheLoad> cachestack;
+
+
+
+	public LinkedList<CacheLoad> getCacheLines() {
+		return cachestack;
+	}
+
+	public void add(long address, WORD data) {
+		if (this.cachestack.size() >= Cache.DEFAULT_CACHE_SIZE) {
+			this.cachestack.removeLast();
+		}
+		this.cachestack.addFirst(new CacheLoad(address, data));
+	}
+
+
 
 	/**
 	 * 
 	 */
-	public Cache() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param address
-	 * @return
-	 */
+   
 	public WORD load(long address) {
 		WORD result=null;
 		return result;
