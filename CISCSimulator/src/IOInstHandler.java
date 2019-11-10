ff67cb6555478848b4244500943cd63bbf6f1659
cozyu-gwu <cozyu@gwu.edu>
2019-11-09 19:31:49 -0500

    minor fix

--- CISCSimulator/src/IOInstHandler.java
+++ CISCSimulator/src/IOInstHandler.java
@@ -2,9 +2,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.io.File; 
 import java.io.FileNotFoundException; 
-import java.util.Scanner; 
 
 
 
