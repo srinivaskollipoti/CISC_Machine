8a523ea6588fde07842dd4492d5290389a8f8973
caoyou222 <caoyou12@gmail.com>
2019-11-21 16:34:05 -0500

    floatingWROD

--- CISCSimulator/src/FloatingWORD.java
+++ CISCSimulator/src/FloatingWORD.java
@@ -3,35 +3,34 @@
  * @author cozyu
  *
  */
-public class FloatingWORD extends WORD {
-
-	/**
-	 * @param length
-	 */
+public class FloatingWORD extends GBitSet {
+	static final int SIZE=6;
 	public FloatingWORD() {
-		super();
+		super(FloatingWORD.SIZE);
 		setMinValue((int)(Math.pow(2, FloatingWORD.SIZE-1)*-1));
-		setMaxValue((int)(Math.pow(2, FloatingWORD.SIZE-1)-1));
-	}
-	
-	public FloatingWORD(WORD word) {
-		super();
-		setMinValue((int)(Math.pow(2, FloatingWORD.SIZE-1)*-1));
-		setMaxValue((int)(Math.pow(2, FloatingWORD.SIZE-1)-1));
-		or(word);
+		setMaxValue((int)(Math.pow(2, FloatingWORD.SIZE)));
 	}
 	
 	public FloatingWORD(java.util.BitSet input)
 	{
-		super();
+		super(FloatingWORD.SIZE);
 		setMinValue((int)(Math.pow(2, FloatingWORD.SIZE-1)*-1));
-		setMaxValue((int)(Math.pow(2, FloatingWORD.SIZE-1)-1));
+		setMaxValue((int)(Math.pow(2, FloatingWORD.SIZE)));
 		or(input);
 	}
-
-	public long getLong(){
-		return (short)super.getLong();
-	}
 	
-
+	public float getFloat() {
+		if(this.isEmpty())
+			return 0;
+		long exp = this.get(2,7).toLongArray()[0];
+		if(this.get(1,1).toLongArray()[0] == 1) exp = -1*exp;
+		long mant = this.get(8,15).toLongArray()[0];
+		float res = (float)(Math.pow(mant, exp-8));
+		if(this.toLongArray()[0] == 1) res = -1*res;
+		return res;
+	}
+	public long setFloat(float f) {
+		
+		return 0;
+	}
 }
