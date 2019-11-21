2b4cc5294dea04c88bdcbd269d289791e7ee0779
caoyou222 <caoyou12@gmail.com>
2019-11-21 00:29:11 -0500

    added documentation, added register FR and STFR, LDFR, FADD.

---
+++ CISCSimulator/src/FloatInstHandler.java
@@ -0,0 +1,156 @@
+import java.io.IOException;
+
+/**
+ * perform floating points and vector operations instruction
+ */
+
+/**
+ * @author cozyu
+ * @author youcao
+ *
+ */
+public class FloatInstHandler extends InstructionHandler {
+
+	/**
+	 * @param cpu
+	 */
+	public FloatInstHandler(CPU cpu) {
+		super(cpu);
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean execute() throws IOException{
+		LOG.info("Execute TRANS Instruction\n");
+		message="";
+		parseIR(cpu.getIR());
+		switch(getOPCode())
+		{
+			case InstructionSet.FADD:
+				executeFADD();
+				break;
+				
+			case InstructionSet.FSUB:
+				executeFSUB();
+				break;
+				
+			case InstructionSet.VADD:
+				executeVADD();
+				break;
+				
+			case InstructionSet.VSUB:
+				executeVSUB();
+				break;
+				
+			case InstructionSet.CNVRT:
+				executeCNVRT();
+				break;
+				
+			case InstructionSet.LDFR:
+				executeLDFR();
+				break;
+				
+			case InstructionSet.STFR:
+				executeSTFR();
+				break;
+				
+			default:
+				message="Unknown Instruction(OPCODE): "+ir;
+				LOG.warning(message);
+				break;
+		}
+		return true;
+	}
+
+	/**
+	 * Add c(EA) to the floating-point register
+	 * @return On case success, true is returned, otherwise false is returned.
+	 */
+	private boolean executeFADD() throws IOException{	
+		int eAddress=getEA();
+		WORD param=new SignedWORD();
+		param.setLong(cpu.loadMemory(eAddress).getLong()+cpu.getFR(reg).getLong());
+		if(param.getLong() > 64) {
+			message+="==> FR"+reg+"+"+cpu.loadMemory(eAddress)+"  = OVERFLOW "+"\n";
+			return true;
+		}
+		cpu.getFR(reg).copy(param);
+		message+="==> FR"+reg+"+"+cpu.loadMemory(eAddress)+" = "+cpu.getFR(reg).getLong()+"\n";
+	    return true;
+	}
+	
+	/**
+	 * Subtract c(EA) from the floating-point register
+	 * @return true
+	 * @throws IOException
+	 */
+	private boolean executeFSUB() throws IOException {
+		
+	    return true;
+	}
+	
+	/**
+	 * Add vectors in EA and EA+1
+	 * @return true
+	 * @throws IOException
+	 */
+	
+	private boolean executeVADD() throws IOException {
+		
+	    return true;
+	}
+	
+	/**
+	 * Subtract vector in EA+1 from EA
+	 * @return
+	 * @throws IOException
+	 */
+	private boolean executeVSUB() throws IOException {
+		
+		return true;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 * @throws IOException
+	 */
+	private boolean executeCNVRT() throws IOException {
+		
+	    return true;
+	}
+	
+	/**
+	 * Load floating register from memory, fr0 = c(EA), fr1 = c(EA+1)
+	 * @return
+	 * @throws IOException
+	 */
+	private boolean executeLDFR() throws IOException {
+		int eAddress=getEA();
+		cpu.getFR(0).copy(cpu.loadMemory(eAddress));
+		cpu.getFR(1).copy(cpu.loadMemory(eAddress));
+		message+="==> FR0"+" = mem["+eAddress+"] = "+cpu.getFR(0).getLong()+"\n";
+		message+="==> FR1"+" = mem["+eAddress+1+"] = "+cpu.getFR(1).getLong()+"\n";
+		return true;
+	}
+	
+	/**
+	 * Store floating register to memory, c(EA) = fr0, c(EA+1) = fr1
+	 * @return
+	 * @throws IOException
+	 */
+	private boolean executeSTFR() throws IOException {
+		int eAddress=getEA();
+		WORD param1=new SignedWORD();
+		WORD param2=new SignedWORD();
+	    param1.copy(cpu.getFR(0));
+	    param2.copy(cpu.getFR(1));
+		cpu.storeMemory(eAddress,param1);
+		cpu.storeMemory(eAddress+1,param2);
+		message+="==>  mem["+eAddress+"] = "+param1.getLong()+"\n";
+		message+="==>  mem["+eAddress+1+"] = "+param2.getLong()+"\n";
+		return true;
+	}
+
+	
+
+}
